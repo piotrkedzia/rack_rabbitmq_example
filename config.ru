4ff5b6bd522aad830e9210635710e15f29c6b58d
Piotr Kędzia <piotr@kedzia.eu>
2016-02-25 13:11:14 +0000

    Add the ability to serve static files

--- config.ru
+++ config.ru
@@ -4,4 +4,8 @@ Bundler.require
 require_relative './config/load_config'
 require './app/amqp_sender_app'
 
+use Rack::Static,
+  :urls => ["/images", "/js", "/css"],
+  :root => "public"
+
 run AmqpSenderApp.new
--- config/load_config.rb
+++ config/load_config.rb
@@ -1,4 +1,4 @@
 require 'yaml'
-
-raw_config = File.read('./config/application.yml')
+file_path = File.expand_path('../application.yml', __FILE__)
+raw_config = File.read(file_path)
 APP_CONFIG = YAML.load(raw_config)[ENV['RACK_ENV']]
